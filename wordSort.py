ef04d34d5a06b934fd8b054b7d3ac35d158280d2
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-04-13 08:42:21 -0400

    Create wordSort.py

---
+++ wordSort.py
@@ -0,0 +1,11 @@
+#Matt Westelman
+#4/14/18
+#listDemo.py - Lists. How hard could this be?
+
+words = input("Enter some words: ").split(" ")
+words.sort()
+
+#printing list 1 item at a time
+for item in words:
+    print(item)
+
