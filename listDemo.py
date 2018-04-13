e37b167add66b66d1a5baecbcb4b52c7f1907cb4
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-04-13 08:36:05 -0400

    Update listDemo.py

--- listDemo.py
+++ listDemo.py
@@ -1,1 +1,13 @@
+#Matt Westelman
+#4/14/18
+#listDemo.py - Lists. How hard could this be?
+
+words = input("Enter some words: ").split(" ")
+
+print("The first word was: ",words[0])
+print("The last word was: ",words[-1])
+
+#printing list 1 item at a time
+for item in words:
+    print(item)
 
