2486b0a43996c842799bb06b419fa113b0b036b7
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-02 10:16:48 -0400

    Create cocktailSort.py

---
+++ cocktailSort.py
@@ -0,0 +1,46 @@
+#Matt Westelman
+#5/2/18
+#cocktailSort.py - implementing coctail sort
+
+from random import randint
+from time import time
+
+N = 10 #how many numbers will be sorted
+
+def mySort(A):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(0, len(A)-1):
+            if A[i] > A[i+1]:
+                A[i], A[i+1] = A[i+1], A[i]
+                swapped = True
+        if not swapped:
+            break
+        swapped = False
+        for i in range(len(A)-2,-1,-1):
+            if A[i] > A[i+1]:
+                A[i], A[i+1] = A[i+1], A[i]
+                swapped = True
+    return A
+    
+if __name__ == '__main__':
+
+    #make a list of N random numbers between 1 and N
+    numbers = [0]*N
+    for i in range(N):
+        numbers[i] = randint(1,N)
+    
+    pythonSort = sorted(numbers) #Python's sort
+    
+    #time how long your sort takes
+    t1 = time()
+    numbers = mySort(numbers)
+    t2 = time()
+    
+    #print whether the sort worked or not
+    try:
+        assert(numbers == pythonSort)
+        print('Your sort took', t2-t1, 'seconds')
+    except:
+        print('Your sort did not work')
