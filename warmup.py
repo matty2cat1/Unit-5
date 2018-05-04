79cc3ae0d0fd1ec965eba823237a0ecfe642a728
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-04 08:46:11 -0400

    Create warmup.py

---
+++ warmup.py
@@ -0,0 +1,11 @@
+#Matt Westelman
+#5/3/18
+#warmUpSomething.py - quickie
+
+def double(L):
+    new = []
+    for item in L:
+        new.append(item*2)
+    return new
+    
+print(double([1, 2 ,3]))
